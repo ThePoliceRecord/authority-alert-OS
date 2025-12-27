ec9298b53f076158c274164cce936f936f224d01
E.J. Bevenour <ej@staff.thepolicerecord.com>
2025-12-27 07:47:29 -0500

    feat(camera-streamer): add sscma-camera-streamer package and config updates

---
+++ external/br2-external/sscma-camera-streamer/sscma-camera-streamer.mk
@@ -0,0 +1,42 @@
+################################################################################
+#
+# sscma-camera-streamer
+#
+################################################################################
+
+SSCMA_CAMERA_STREAMER_VERSION = development
+SSCMA_CAMERA_STREAMER_SITE = https://github.com/ThePoliceRecord/sscma-example-sg200x
+SSCMA_CAMERA_STREAMER_SITE_METHOD = git
+SSCMA_CAMERA_STREAMER_GIT_SUBMODULES = YES
+SSCMA_CAMERA_STREAMER_LICENSE = Apache-2.0
+
+# Configure step: prepare the build environment and run CMake to configure the build
+define SSCMA_CAMERA_STREAMER_CONFIGURE_CMDS
+	mkdir -p $(@D)/solutions/camera-streamer/build && \
+	cd $(@D)/solutions/camera-streamer/build && \
+	$(BR2_CMAKE) \
+		-DSG200X_SDK_PATH=$(O)/../../../ \
+		-DSYSROOT=$(STAGING_DIR) \
+		-DCMAKE_BUILD_TYPE=Release \
+		-DCMAKE_INSTALL_PREFIX=$(TARGET_DIR) \
+		..
+endef
+
+# Build step: compile the package using the Makefile in the build directory
+define SSCMA_CAMERA_STREAMER_BUILD_CMDS
+	$(MAKE) -C $(@D)/solutions/camera-streamer/build
+endef
+
+# Install step: copy the built binary and rootfs files to the target directory
+define SSCMA_CAMERA_STREAMER_INSTALL_TARGET_CMDS
+	# Install the executable file
+	$(INSTALL) -D -m 0755 $(@D)/solutions/camera-streamer/build/camera-streamer \
+		$(TARGET_DIR)/usr/bin/camera-streamer
+	
+	# Copy rootfs files if they exist
+	if [ -d $(@D)/solutions/camera-streamer/rootfs ]; then \
+		cp -r $(@D)/solutions/camera-streamer/rootfs/* $(TARGET_DIR)/; \
+	fi
+endef
+
+$(eval $(generic-package))
