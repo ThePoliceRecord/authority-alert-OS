6657468f2c6e7929fdebf6e9a165b844827b41a6
E.J. Bevenour <ejb1123@protonmail.com>
2025-12-31 17:14:44 -0500

    fixed sdcard mounting and unmounting (#9)
    Co-authored-by: E.J. Bevenour <ej@staff.thepolicerecord.com>

--- external/buildroot/board/cvitek/CV181X/overlay/etc/hotplug/automount.sh
+++ external/buildroot/board/cvitek/CV181X/overlay/etc/hotplug/automount.sh
@@ -1,26 +1,21 @@
 #!/bin/sh
 
-MOUNT_POINT=/tmp/sd
-LINK_POINT=/mnt/sd
+MOUNT_POINT=/mnt/sd
 
 # echo "<<<<<<<<<<<<<$1 $2>>>>>>>>>>>>>"
 if [ "$1" == "mmcblk1p1" ]; then
   disk="/dev/$1"
   if [ "$2" = "add" ]; then
+      rootfs_rw on
       mkdir -p $MOUNT_POINT
+      rootfs_rw off
+
       umount $MOUNT_POINT
       mount $disk $MOUNT_POINT
-
-      rootfs_rw on
-      if [ -L $LINK_POINT ]; then
-        rm $LINK_POINT
-      fi
-      ln -s $MOUNT_POINT /mnt/
-      rootfs_rw off
   else
       umount $MOUNT_POINT
       rootfs_rw on
-      rm $LINK_POINT
+      rmdir $MOUNT_POINT
       rootfs_rw off
   fi
 fi
